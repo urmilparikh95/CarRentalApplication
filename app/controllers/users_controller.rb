9ca31eb9e26c97adcf5f671ea0608a6585094b4f
Urmil Parikh <uparikh@ncsu.edu>
2017-09-23 15:02:20 -0400

    user updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :first_name, :last_name, :password)
+      params.require(:user).permit(:email, :first_name, :last_name, :password, :role_id)
     end
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :email, :first_name, :last_name, :password, :created_at, :updated_at
+json.extract! user, :id, :email, :first_name, :last_name, :password, :role_id, :created_at, :updated_at
 json.url user_url(user, format: :json)
