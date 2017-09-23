3bc96a9fdaa0af9cdffd4e424790f56347fbb373
Sandeep Hegde <sdhegde@ncsu.edu>
2017-09-23 15:51:50 -0400

    Added enum status to car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,6 @@
 class Car < ApplicationRecord
+  enum status: { available: 'available', reserved: 'reserved', checked_out: 'checked out' }
+
   has_many :reservations
   has_many :users, through: :reservations
 end
