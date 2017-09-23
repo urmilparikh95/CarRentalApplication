37e8f906425531fcc5cdc7e12182194d1b515756
Sandeep Hegde <sdhegde@ncsu.edu>
2017-09-23 17:14:11 -0400

    Added validations for car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,11 @@
 class Car < ApplicationRecord
-  enum status: { available: 'available', reserved: 'reserved', checked_out: 'checked out' }
-
+  enum status: { available: 'Available', reserved: 'Reserved', checked_out: 'Checked out' }
+  enum style: { coupe: 'Coupe', sedan: 'Sedan', suv: 'SUV' }
   has_many :reservations
   has_many :users, through: :reservations
+
+  validates :licence_no, :manufacturer, :model, :style, :hourly_rate, :location, :status, presence: true
+  validates :licence_no, length: { is: 7 }
+  validates :licence_no, uniqueness: true
+  validates :hourly_rate, numericality: { greater_than_or_equal_to: 0 }
 end
