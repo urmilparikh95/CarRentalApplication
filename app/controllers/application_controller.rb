f712fcfd33e037f8d54bc3743661bca03dadabc2
uparikh <uparikh@ncsu.edu>
2017-09-23 20:49:52 -0400

    commented those 2 functions as they were creating problem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    #@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def require_user
-    redirect_to login_path unless current_user
+    #redirect_to login_path unless current_user
   end
 end
