6a396e514fef6583abc066def764dd554caf0b08
Sandeep Hegde <sdhegde@ncsu.edu>
2017-09-24 15:17:35 -0400

    schema changes for role

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20170924132709) do
   end
 
   create_table "roles", force: :cascade do |t|
-    t.string "role_name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :role do
-    role_name "MyString"
+    name "MyString"
   end
 end
