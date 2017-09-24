12b7ba5c905135cb265c9130f51a30cebda6ebac
Sandeep Hegde <sdhegde@ncsu.edu>
2017-09-24 01:15:41 -0400

    Added model methods to check role of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   belongs_to :role
   validates :email, :first_name, :last_name, :role_id, presence: true
   validates :email, uniqueness: true
+
+  def admin?
+    current_user.role.name == 'Admin'
+  end
+
+  def super_admin?
+    current_user.role.name == 'SuperAdmin'
+  end
+
+  def customer?
+    current_user.role.name == 'Customer'
+  end
 end
--- db/migrate/20170923155702_create_roles.rb
+++ db/migrate/20170923155702_create_roles.rb
@@ -1,7 +1,7 @@
 class CreateRoles < ActiveRecord::Migration[5.1]
   def change
     create_table :roles do |t|
-      t.string :role_name
+      t.string :name
 
       t.timestamps
     end
