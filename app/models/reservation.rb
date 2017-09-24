78ff5882f2b8da92176ec7fd43c34da922d4280e
Sandeep Hegde <sdhegde@ncsu.edu>
2017-09-24 00:02:10 -0400

    Added validations for reseravation table

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,33 @@
 class Reservation < ApplicationRecord
   belongs_to :car
   belongs_to :user
+
+  validate :booking_time
+  validate :booking_within_seven_days
+  validate :valid_time
+
+  def booking_time
+    return if (to - from) / 1.hour >= 1 and (to - from) / 1.hour <= 10
+    if to <= from
+      errors.add(:base, 'To date should be larger than from date')
+    elsif (to - from) / 1.hour < 1
+      errors.add(:base, 'Booking should be made for more than 1 hour')
+    else
+      errors.add(:base, 'Booking should be made for less than 10 hour')
+    end
+  end
+
+  def booking_within_seven_days
+    return if ((from - DateTime.now) / 1.day) < 7
+    errors.add(:base, 'Reservation within 7 days are only possible')
+  end
+
+  def valid_time
+    return if from > DateTime.now and to > DateTime.now
+    errors.add(:base, 'Time should be more than current time')
+  end
+
+  def calculate_rental_charge
+    ((to - from) / 1.hour) * car.hourly_rate
+  end
 end
