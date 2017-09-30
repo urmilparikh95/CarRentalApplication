1759a8eecec4fe5ddb090921feba7c5c05c46c84
Sandeep Hegde <sdhegde@ncsu.edu>
2017-09-30 02:13:51 -0400

    Super Admin, User controller changes.

--- app/controllers/super_admin/users_controller.rb
+++ app/controllers/super_admin/users_controller.rb
@@ -2,39 +2,38 @@ class SuperAdmin::UsersController < SuperAdmin::SuperAdminController
   skip_before_action :require_user, only: [:new, :create]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
+  # GET super_admin/users
+  # GET super_admin/users.json
   def index
-    @users = User.all
+    role = Role.find_by_name('SuperAdmin')
+    @users = role.users
   end
 
-  # GET /users/1
-  # GET /users/1.json
+  # GET super_admin/users/1
+  # GET super_admin/users/1.json
   def show
-   @users = User.find(params[:id])
   end
 
-  # GET /users/new
+  # GET super_admin/users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
+  # GET super_admin/users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
+  # POST super_admin/users
+  # POST super_admin/users.json
   def create
     @user = User.new(user_params)
-    if(!@user.role_id)
-      @user.role = Role.find_by_name("Customer")
+    unless @user.role
+      @user.role = Role.find_by_name('SuperAdmin')
     end
 
     respond_to do |format|
       if @user.save
-        session[:user_id] = @user.id
-        format.html { redirect_to @user, notice: 'Welcome to Car Rental Application.' }
+        format.html { redirect_to super_admin_users_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -43,12 +42,12 @@ class SuperAdmin::UsersController < SuperAdmin::SuperAdminController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+  # PATCH/PUT super_admin/users/1
+  # PATCH/PUT super_admin/users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to super_admin_users_path, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -57,12 +56,12 @@ class SuperAdmin::UsersController < SuperAdmin::SuperAdminController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
+  # DELETE super_admin/users/1
+  # DELETE super_admin/users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to super_admin_users_path, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -75,6 +74,6 @@ class SuperAdmin::UsersController < SuperAdmin::SuperAdminController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation, :role_id)
+      params.require(:user).permit(:email, :first_name, :last_name, :password, :role_id)
     end
 end
