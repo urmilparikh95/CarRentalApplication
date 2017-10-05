1d46e5f82991f4fbf2378ade02d56e10eccee728
Urmil Parikh <uparikh@ncsu.edu>
2017-10-04 20:57:30 -0400

    seeds added for multiple cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,14 @@
 Role.create(name: 'SuperAdmin')
 Role.create(name: 'Admin')
 Role.create(name: 'Customer')
-Car.create(licence_no: 'abc1234', manufacturer: 'Nissan', model: 'Sentra', style: 'Sedan', hourly_rate: '10', location: 'Raleigh', status: 'Available')
-Car.create(licence_no: 'abc8765', manufacturer: 'Tesla', model: 'S', style: 'Sedan', hourly_rate: '20', location: 'Raleigh', status: 'Reserved')
-Car.create(licence_no: 'abc7777', manufacturer: 'Tesla', model: 'X', style: 'SUV', hourly_rate: '30', location: 'Durham', status: 'Available')
-Car.create(licence_no: 'abc8888', manufacturer: 'Honda', model: 'Civic', style: 'Sedan', hourly_rate: '5', location: 'Raleigh', status: 'Checked out')
 User.create(email: 'admin@gmail.com', first_name: 'Mr.', last_name: 'Admin', password: 'admin123', password_confirmation: 'admin123', role: Role.find_by_name('Admin'))
 User.create(email: 'superadmin@gmail.com', first_name: 'Mr.', last_name: 'SuperAdmin', password: 'supadmin123', password_confirmation: 'supadmin123', role: Role.find_by_name('SuperAdmin'))
+10.times do
+  licence_no = [:abc, :xyz, :pqr, :lmn, :qwe].sample.to_s + rand(1000...9999).to_s
+  manufacturer = [:Nissan, :Tesla, :Honda, :Hyundai, :Dodge, :Mercedes, :Audi, :Toyota, :Ford, :Volkswagen].sample.to_s
+  model = [:Civic, :"Model X", :"Model S", :A8, :Viper, :Elentra, :Corolla, :CRV, :Sentra, :Passat, :Polo].sample.to_s
+  style = [:Coupe, :Sedan, :SUV].sample.to_s
+  rate = rand(30...100)
+  location = [:Raleigh, :Cary, :Durham, :Charlotte, :Ashville].sample.to_s
+  Car.create(licence_no: licence_no, manufacturer: manufacturer, model: model, style: style, hourly_rate: rate, location: location, status: 'Available')
+end
